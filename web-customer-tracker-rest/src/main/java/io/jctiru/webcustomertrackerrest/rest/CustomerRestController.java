4ecdc8fe8de54eabb9118b8ffc25c592e5ebbda4
jctiru <jonathanchristiantiru@gmail.com>
2019-09-20 21:26:31 +0800

    Add create customer endpoint

--- web-customer-tracker-rest/src/main/java/io/jctiru/webcustomertrackerrest/rest/CustomerRestController.java
+++ web-customer-tracker-rest/src/main/java/io/jctiru/webcustomertrackerrest/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +32,17 @@ public class CustomerRestController {
 		if (customer == null) {
 			throw new CustomerNotFoundException("Customer id not found: " + customerId);
 		}
+
+		return customer;
+	}
+
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer customer) {
+		// .saveOrUpdate() checks the id if it is empty for creating new item else
+		// update item based on id
+		// Also to force a save of new item in case an id is passed
+		customer.setId(0);
+		customerService.saveCustomer(customer);
 
 		return customer;
 	}
