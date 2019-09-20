aa3d55e6109cafcd692e757a4d451f3dcd445721
jctiru <jonathanchristiantiru@gmail.com>
2019-09-20 21:39:13 +0800

    Add update customer endpoint

--- web-customer-tracker-rest/src/main/java/io/jctiru/webcustomertrackerrest/rest/CustomerRestController.java
+++ web-customer-tracker-rest/src/main/java/io/jctiru/webcustomertrackerrest/rest/CustomerRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +43,13 @@ public class CustomerRestController {
 		// update item based on id
 		// Also to force a save of new item in case an id is passed
 		customer.setId(0);
+		customerService.saveCustomer(customer);
+
+		return customer;
+	}
+
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer customer) {
 		customerService.saveCustomer(customer);
 
 		return customer;
